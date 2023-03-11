c56741ee009a8bc8e37dae42a2e417b099d9dc0c
Hakan Gul <hakanngul@icloud.com>
2023-03-11 14:16:59 +0300

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Hakan Gül on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
